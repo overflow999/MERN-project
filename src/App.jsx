771adea4bfcc72ce1239ab5d495f9af97aa09084
Your Name <claudelpierre786@.com>
2024-01-16 14:31:41 -0500

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from "react"
+import { BrowserRouter,Route,Routes } from "react-router-dom"
+import About from './pages/About'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import Home from './pages/Home'
+import Logout from './pages/Logout'
+
 
 const App = () => {
-  return (
-    <div className='text-red-500
-    '>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}/>   
+    <Route path="/Login" element={<Login/>}/>   
+    <Route path="/SignUp" element={<SignUp/>}/>   
+    <Route path="/About" element={<About/>}/>   
+    <Route path="/Profile" element={<Profile/>}/>   
+    <Route path="/Logout" element={<Logout/>}/>   
+
+  </Routes> 
+ </BrowserRouter>
 }
 
 export default App
